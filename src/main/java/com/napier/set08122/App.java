42691ff4ff9d06e089f28372d78af06270441802
Dillon Aitken <dillonaitken87@gmail.com>
2022-02-03 12:40:33 +0000

    Add placeholder game grid. Define build configuration.

---
+++ src/main/java/com/napier/set08122/App.java
@@ -0,0 +1,26 @@
+package com.napier.set08122;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Napier Sudoku");
+
+        System.out.println("    A   B   C     D   E   F     H   I   J");
+        System.out.println("  ┌───┬───┬───┐ ┌───┬───┬───┐ ┌───┬───┬───┐");
+        System.out.println("1 | X | X | X | | X | X | X | | X | X | X |");
+        System.out.println("2 | X | X | X | | X | X | X | | X | X | X |");
+        System.out.println("3 | X | X | X | | X | X | X | | X | X | X |");
+        System.out.println("  └───┴───┴───┘ └───┴───┴───┘ └───┴───┴───┘");
+        System.out.println("  ┌───┬───┬───┐ ┌───┬───┬───┐ ┌───┬───┬───┐");
+        System.out.println("4 | X | X | X | | X | X | X | | X | X | X |");
+        System.out.println("5 | X | X | X | | X | X | X | | X | X | X |");
+        System.out.println("6 | X | X | X | | X | X | X | | X | X | X |");
+        System.out.println("  └───┴───┴───┘ └───┴───┴───┘ └───┴───┴───┘");
+        System.out.println("  ┌───┬───┬───┐ ┌───┬───┬───┐ ┌───┬───┬───┐");
+        System.out.println("7 | X | X | X | | X | X | X | | X | X | X |");
+        System.out.println("8 | X | X | X | | X | X | X | | X | X | X |");
+        System.out.println("9 | X | X | X | | X | X | X | | X | X | X |");
+        System.out.println("  └───┴───┴───┘ └───┴───┴───┘ └───┴───┴───┘");
+    }
+}
